cc1c82d8c270927e53bf787b6954e8e0f7c5b4ad
hon chau <chaugeorge9@gmail.com>
2024-12-27 21:37:35 -0600

    did Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+const Navbar = () =>{
+    const [open, setOpen] = useState(false);
+    return{
+
+        <div className="w-full h-16 md:h-20 flex items-center justify-between">
+        // LOGO
+            <div className="flex items-center gap-4 text-2xl font-bold">
+                <img src="logo.png" className="w-8 h-8" alt="" />
+                <span>lamalog</span>
+                </div>
+        {/*MOBILE MENU */}        
+        <div className="md:hidden">M</div>
+        <div className="cursor-pointer text-4xl" onClick={() =>setOpen((prev)=> !prev)}> 
+        {open ? "x":"m"}</div>
+
+        </div>
+MOBILE LINK LIST
+        {/*Desktop MENU */}  
+        <div className="hidden md:flex">D</div>
+        
+        </div>
+       
+
+    }
+}
+export default Navbar;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,20 +9,49 @@ const Navbar = () =>{
             <div className="flex items-center gap-4 text-2xl font-bold">
                 <img src="logo.png" className="w-8 h-8" alt="" />
                 <span>lamalog</span>
-                </div>
+            </div>
         {/*MOBILE MENU */}        
-        <div className="md:hidden">M</div>
-        <div className="cursor-pointer text-4xl" onClick={() =>setOpen((prev)=> !prev)}> 
-        {open ? "x":"m"}</div>
+        <div className="md:hidden">
+        // MOBILE BUTTON
+        </div>
+
+        <div 
+        className="cursor-pointer text-4xl" onClick={() =>setOpen((prev)=> !prev)}> 
+        {open ? "x":"m"}
 
         </div>
-// MOBILE LINK LIST
-        {/*Desktop MENU */}  
-        <div className="hidden md:flex">D</div>
         
         </div>
-       
-
+        // MOBILE LINK LIST
+        <div  className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-absolute top-16 
+        bg-red-700 transition-all ease-in-out ${open ? "-right-0":"-right-[100%]"}`}>
+        menu
+        </div>
+        
+        
+        <div className="hidden md:flex items-center group">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+        <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login
+        </button></a>
+        
+        </div>
+        
+        </div>
+        {/*Desktop MENU */}  
+        <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+        <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 
+        </button>
+        </a></div>
+        </div>
     }
 }
 export default Navbar;
