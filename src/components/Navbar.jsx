d237b3f441e48ac35ce268fd6affd4fd5f2af66c
hon chau <chaugeorge9@gmail.com>
2024-12-28 19:11:25 -0600

    modify  Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () =>{
 
         <div className="w-full h-16 md:h-20 flex items-center justify-between">
         // LOGO
-            <div className="flex items-center gap-4 text-2xl font-bold">
+            <Link to='/' className="flex items-center gap-4 text-2xl font-bold">
                 <img src="logo.png" className="w-8 h-8" alt="" />
                 <span>lamalog</span>
-            </div>
+            </Link>
         {/*MOBILE MENU */}        
         <div className="md:hidden">
         // MOBILE BUTTON
@@ -30,27 +30,27 @@ const Navbar = () =>{
         
         
         <div className="hidden md:flex items-center group">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="">
         <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login
-        </button></a>
+        </button></Link>
         
         </div>
         
         </div>
         {/*Desktop MENU */}  
         <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="">
         <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 
         </button>
-        </a></div>
+        </Link></div>
         </div>
     }
 }
---
+++ src/layouts/Mainlayout.jsx
@@ -0,0 +1,13 @@
+const Mainlayout = () =>{
+    return(
+        <div className='px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64'>
+            <Navbar/>
+
+        </div>
+    )
+
+
+
+}
+
+export default MainLayout
